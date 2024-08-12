91a8229ffeaf12765443dea3dad8399b56721bcf
Ivan-Efimov52 <iv.efimov52@yandex.ru>
2024-08-12 15:29:14 +0300

    Иван Ефимов сайт

---
+++ app.js
@@ -0,0 +1,23 @@
+VANTA.WAVES({
+    el: "#intro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3c5391
+  })
+
+  VANTA.WAVES({
+    el: "#outro",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00,
+    color: 0x3c5391
+  })
